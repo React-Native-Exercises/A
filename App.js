80e21273bec91d323bacfa7aa42d1999a4726abe
ReidHolmes <name.reidholmes@gmail.com>
2019-02-18 12:21:18 +0800

    Add

---
+++ App.js
@@ -0,0 +1,63 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+/*import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';*/
+import React, { Component } from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+
+/*
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+*/
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    var movie = MOCKED_MOVIES_DATA[0];
+    return (
+        <View style={styles.container}>
+          <Image source={{ uri: movie.posters.thumbnail }} style={styles.thumbnail} />
+          <View style={styles.rightContainer}>
+            <Text style={styles.title}>{movie.title}</Text>
+            <Text style={styles.year}>{movie.year}</Text>
+          </View>
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81
+  }
+
+
+});
+
+
+var MOCKED_MOVIES_DATA = [
+  {
+    title: "标题",
+    year: "2015",
+    posters: { thumbnail: "http://i.imgur.com/UePbdph.jpg" }
+  }
+];
